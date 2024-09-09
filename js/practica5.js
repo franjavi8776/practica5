cc98701ddf51ee1b46ebf4b33dfdfbb4380834c1
franjavi8776 <franjavi871976@gmail.com>
2024-09-09 17:37:42 -0400

    first commit

---
+++ js/practica5.js
@@ -0,0 +1,10 @@
+/* Hacer un CRUD:
+    C (create) = ingresar un color por el input.
+    R (read) = renderizar el color dentro de la ul en una li.
+    U (update) = dentro del li crear un boton para actualizar y hacer que funcione.
+    D (delete) = dentro del li crear un boton para eliminar y hacer que funcione.
+    
+    -Utilizar el localstorage para emular una base de datos donde se guarden los colores y se renderizen.
+
+    -Por ultimo subir la carpeta practicaJs5 a su github personal.
+*/
